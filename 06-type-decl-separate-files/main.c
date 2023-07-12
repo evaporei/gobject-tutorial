eefb107cda6d06c966b936e4934903cff4fac44a
Eva Pace <eba.pachi@gmail.com>
2023-07-12 11:46:37 -0300

    06: simplify main.c printing

--- 06-type-decl-separate-files/main.c
+++ 06-type-decl-separate-files/main.c
@@ -2,85 +2,66 @@
 
 #include "tdouble.h"
 
+void t_double_print (TDouble *d, char *op) {
+  double value;
+  if (t_double_get_value (d, &value)) {
+    g_print ("successful %s: %lf\n", op, value);
+  } else {
+    g_print ("%s failed\n", op);
+  }
+}
+
 void math() {
   TDouble *d1, *d2, *d3;
-  double value3;
 
   d1 = t_double_new (25.0);
   d2 = t_double_new (-14.0);
 
+  // 25 - 14 = 11
   d3 = t_double_add (d1, d2);
-  if (t_double_get_value (d3, &value3)) {
-    g_print ("successful add: %lf\n", value3);// 25 - 14 = 11
-  } else {
-    g_print ("add failed\n");
-  }
+  t_double_print (d3, "add");
 
   g_object_unref (d3);
 
+  // 25 + 14 = 39
   d3 = t_double_sub (d1, d2);
-  if (t_double_get_value (d3, &value3)) {
-    g_print ("successful sub: %lf\n", value3);// 25 + 14 = 39
-  } else {
-    g_print ("sub failed\n");
-  }
+  t_double_print (d3, "sub");
 
   g_object_unref (d3);
 
+  // 25 * (-14) = -350
   d3 = t_double_mul (d1, d2);
-  if (t_double_get_value (d3, &value3)) {
-    g_print ("successful mul: %lf\n", value3);// 25 * (-14) = -350
-  } else {
-    g_print ("mul failed\n");
-  }
+  t_double_print (d3, "mul");
 
   g_object_unref (d3);
 
+  // 25 / (-14) = -1.785714
   d3 = t_double_div (d1, d2);
-  if (t_double_get_value (d3, &value3)) {
-    g_print ("successful div: %lf\n", value3);// 25 / (-14) = -1.785714
-  } else {
-    g_print ("div failed\n");
-  }
+  t_double_print (d3, "div");
 
   g_object_unref (d3);
 
+  // -25
   d3 = t_double_inv (d1);
-  if (t_double_get_value (d3, &value3)) {
-    g_print ("successful inv1: %lf\n", value3);// -25
-  } else {
-    g_print ("inv1 failed\n");
-  }
+  t_double_print (d3, "inv");
 
   g_object_unref (d3);
 
+  // 14
   d3 = t_double_inv (d2);
-  if (t_double_get_value (d3, &value3)) {
-    g_print ("successful inv2: %lf\n", value3);// 14
-  } else {
-    g_print ("inv2 failed\n");
-  }
+  t_double_print (d3, "inv2");
 }
 
 int main() {
   g_print("main\n");
 
   TDouble *d;
-  double value;
 
   d = t_double_new (10.0);
-  if (t_double_get_value (d, &value)) {
-    g_print ("successful get_value: %lf\n", value);
-  } else {
-    g_print ("get_value failed\n");
-  }
+  t_double_print(d, "get_value");
 
   t_double_set_value (d, -20.0);
-  if (t_double_get_value (d, &value)) {
-    g_print ("successful get_value after set: %lf\n", value);
-  } else {
-    g_print ("get_value failed after set\n");
-  }
+  t_double_print(d, "get_value after set");
 
   math();
 }
